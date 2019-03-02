1c208c0188561b2af0d6fe45350972ea64471d42
bmusick <bmuze1@gmail.com>
2019-03-02 04:44:07 -0700

    setTurn, setLetter, play, prompt

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,10 +1,15 @@
 class TicTacToe
 
-    attr_accessor :board_size, :board
+    attr_accessor :board_size, :board, :current_player
+    attr_reader :player_one, :player_two
 
     def initialize
         @board_size = 5
         @board = Array.new(board_size) {Array.new(board_size)} # Holds instantiated board
+        
+        @player_one = "O"
+        @player_two = "X"
+        @current_player = @playerOne
     end
 
     def setBoard 
@@ -38,6 +43,46 @@ class TicTacToe
             end
         end
         puts board_str
+    end
+
+    def prompt
+        # Prompt the user to enter a coordinate
+
+        # Below is a hash which is used to set the appropriate grid space
+        place = {"TL":self.board[0][0], "TC":self.board[0][2], "TR":self.board[0][4],
+                "ML":self.board[2][0], "MC":self.board[2][2], "MR":self.board[2][4],
+                "BL":self.board[4][0], "BC":self.board[4][2], "BR":self.board[4][4]}
+        
+        choice = ""
+        while choice == ""
+            # While a choice hasnt been made
+            puts "Pick an empty space. Top-right = "TR", bottom-left = "BL", etc. "
+            choice = gets.chomp # Takes user input, removes the newline character (chomp)
+            if !has.keys.include?(choice)
+                # If the choice isnt a key in place hash then keep loop going
+                choice == ""
+            end
+        end
+        self.setLetter(choice,self.current_player) # Set the letter on the board
+        self.setTurn # Change turn to new person
+
+    end
+
+    def setLetter(pos,letter)
+        pos = letter
+    end
+
+    def setTurn
+        # Sets the turn to the person who is not the current player
+        if self.current_player == self.player_one
+            self.current_player = self.player_two
+        else
+            self.current_player = self.player_one 
+        end
+    end
+    def play 
+        # Play a round of tictactoe
+        # This ends when all spaces in grid dont have empty string
     end
 end
 
